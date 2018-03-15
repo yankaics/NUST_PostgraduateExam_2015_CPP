ab0008b032f9158d3c0f65ab72fa4757e1f4c88c
csbhr <931937713@qq.com>
2018-03-15 10:23:23 +0800

    update t4

--- t4.cpp
+++ t4.cpp
@@ -2,17 +2,40 @@
 
 using namespace std;
 
+#define M 10
+
+//计算x=x+y 
+void Multi(int x[M],int y[M]){
+	int postIn=0;
+	int i=0;
+	while(y[i]!=0){
+		int temp=x[i]+y[i]+postIn;
+		x[i]=temp%10;
+		postIn=temp/10;
+		i++;
+	}
+	if(postIn!=0) x[i]=postIn;
+}
+
 int main(){
+	int x[M],y[M];
+	for(int i=0;i<M;i++){
+		x[i]=0;
+		y[i]=0;
+	}
 	int a,n;
 	cin>>a>>n;
-	int sum=0;
-	for(int i=1;i<=n;i++){
-		int tuple=0;
-		for(int j=0;j<i;j++){
-			tuple=tuple*10+a;
-		}
-		sum+=tuple;
+	for(int i=0;i<n;i++){
+		y[i]=a;
+		Multi(x,y);
 	}
-	cout<<sum<<endl;
+	//下面倒序输出x 
+	int stack[M],top=-1;
+	int flag=0;
+	while(x[flag]!=0){
+		stack[++top]=x[flag];
+		flag++;
+	}
+	while(top!=-1) cout<<stack[top--];
 	return 0;
 }
